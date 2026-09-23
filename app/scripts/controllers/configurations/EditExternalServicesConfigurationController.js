746bc3c65180934fb36ef383b317fe5d59ee4910
jjafar <jjafardev@gmail.com>
2026-09-23 16:41:47 +0300

    updated controller>configurations2 coz collection commit was wrongly named config insrtead of collection updated

--- app/scripts/controllers/configurations/EditExternalServicesConfigurationController.js
+++ app/scripts/controllers/configurations/EditExternalServicesConfigurationController.js
@@ -1,46 +1,38 @@
 /**
- * Created by 27 on 04-08-2015.
+ * Created by 27 on 03-08-2015.
  */
 (function (module) {
     mifosX.controllers = _.extend(module, {
-        EditExternalServicesConfigurationController: function ($scope, resourceFactory, $routeParams, location) {
-            $scope.formData = [];
-            $scope.names = [];
-            $scope.externalServicesType = $routeParams.externalServicesType;
-            //$scope.name = $routeParams.name;
-            var nvObject = {};
-            resourceFactory.externalServicesResource.get({id: $scope.externalServicesType}, function (data) {
+        ExternalServicesController: function (scope, resourceFactory, location, route) {
+            scope.S3Configs = [];
+            scope.SMTPConfigs = [];
+            resourceFactory.externalServicesS3Resource.get(function (data) {
                 for (var i in data) {
                     if(data[i] != null && data[i].name != null) {
                         data[i].name.replace(/ /g, '');
                         if (!angular.equals(data[i].name, "")) {
-                            nvObject[data[i].name] = data[i].value;
-                            $scope.names.push(data[i].name);
-
+                            data[i].showEditvalue = true;
+                            scope.S3Configs.push(data[i])
                         }
                     }
                 }
-                $scope.formData = nvObject;
-
-
             });
-
-            $scope.cancel = function () {
-                location.path('/externalservices/'+ $scope.externalServicesType);
-            };
-
-            $scope.submit = function () {
-
-                //scope.formData1 = {  scope.name : scope.formData.value};
-                resourceFactory.externalServicesResource.put({id: $scope.externalServicesType}, this.formData, function (data) {
-                    location.path('/externalservices' );
-                });
-            };
+            resourceFactory.externalServicesSMTPResource.get(function (data) {
+                for (var i in data) {
+                    //console.log(data[0]);
+                    if(data[i].name != "") {
+                        data[i].showEditvalue = true;
+                        scope.SMTPConfigs.push(data[i])
+                    }
+                }
+            });
         }
 
     });
-    mifosX.ng.application.controller('EditExternalServicesConfigurationController', ['$scope', 'ResourceFactory', '$routeParams', '$location', mifosX.controllers.EditExternalServicesConfigurationController]).run(function ($log) {
-        $log.info("EditExternalServicesConfigurationController initialized");
+    mifosX.ng.application.controller('ExternalServicesController', ['$scope', 'ResourceFactory', '$location', '$route',
+        mifosX.controllers.ExternalServicesController]).run(function ($log){
+        $log.info("ExternalServicesController initialized");
     });
+
 
 }(mifosX.controllers || {}));
