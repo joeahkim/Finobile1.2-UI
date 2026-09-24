23f3a15970301e0898234c399f39aaf7e23c46d0
jjafar <jjafardev@gmail.com>
2026-09-24 08:32:59 +0300

    updated controller>deposits

--- app/scripts/controllers/deposits/fixed/ViewFixedDepositAccountDetailsController.js
+++ app/scripts/controllers/deposits/fixed/ViewFixedDepositAccountDetailsController.js
@@ -104,7 +104,6 @@
                 scope.convertDateArrayToObject('date');
                 scope.chartSlabs = scope.savingaccountdetails.accountChart.chartSlabs;
                 scope.status = data.status.value;
-                scope.heading = (!scope.savingaccountdetails.status.rejected && !scope.savingaccountdetails.status.submittedAndPendingApproval)?'label.heading.interestchart':'label.heading.summary';
                 if (scope.status == "Submitted and pending approval" || scope.status == "Active" || scope.status == "Approved") {
                     scope.choice = true;
                 }
